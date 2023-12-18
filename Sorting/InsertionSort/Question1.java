6cb79f9dbd427917ea538af95c1b3927fbf8f971
ajayayrekaraj <ajayayrekaraj@gmail.com>
2023-12-19 01:01:52 +0530

    Insertion Sort Code

---
+++ Sorting/InsertionSort/Question1.java
@@ -0,0 +1,23 @@
+//arr[8,3,1,7,5,4,2]
+
+class InsertionSort{
+	public static void main(String[]args){
+		int arr[]=new int[]{8,3,1,7,5,4,2};
+
+		for(int i=1;i<arr.length;i++){
+			int element=arr[i];
+			int j=i-1;
+
+			while(j>=0 && arr[j]>element){
+				arr[j+1]=arr[j];
+				j--;
+			}
+			arr[j+1]=element;
+		}
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+ " ");
+		}
+		System.out.println();
+	}
+}
+
